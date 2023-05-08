bee0a717c81f22d950a202003b56ddd1e3a1e3c6
Revan707 <ravangh@code.edu.az>
2023-05-08 10:16:45 +0400

    Task is done

---
+++ main.js
@@ -0,0 +1,40 @@
+//Task1
+// let data="Robin Singh"
+// console.log(data.split(" "));
+
+
+//Task2
+// let data="Robin Singh"
+// console.log(data.slice(data,4));
+
+//Task3
+// function hideemail(data) {
+//     let [data_left,data_right]=data.split("@");
+//     let middle=data_left.slice(0,5);
+//     let res=middle+"...@"+data_right;
+//     return res;   
+// }
+
+// console.log(hideemail("revan.huseynov@gmail.com"));
+
+
+
+//Task4
+// function repeatData(data,count){
+//     if(isNaN(count)||count==0){
+//         console.log("Error in string or count.");
+//     }
+//     return data.repeat(count)
+// }
+// console.log(repeatData("a",4))
+
+
+
+
+// Task5
+// let data1='The quick brown fox,fox,ruff,fox'
+// let data2="aa, bb, cc, dd, aa"  
+// const res=data1.match(/fox/g)
+// const res2=data2.match(/aa/g)
+// console.log(res+" "+"was"+" "+"found"+" "+res.length+" "+"times");
+// console.log(res2+" "+"was"+" "+"found"+" "+res2.length+" "+"times");
